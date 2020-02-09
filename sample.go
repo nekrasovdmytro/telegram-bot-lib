9cb45d382f7ccc836d26df19131eacb076fd713c
Dmytro Nekrasov <dmytro.nekrasov@sixt.com>
2020-02-09 17:19:14 +0100

    add chatId

--- sample.go
+++ sample.go
@@ -1,29 +1,30 @@
 package telegrabotlib
 
 type SampleTask struct {
-    Do Executable
+	Do Executable
 }
 
 func (d *SampleTask) Execute(input Input) *TaskResult {
-    if d == nil {
-        return InvalidTaskResult
-    }
+	if d == nil {
+		return InvalidTaskResult
+	}
 
-    return d.Do(input)
+	return d.Do(input)
 }
 
 type TextInput struct {
-    UserId int
-    Username string
-    Text string
-    Location
+	UserId   int
+	Username string
+	ChatId   string
+	Text     string
+	Location
 }
 
 func (d *TextInput) InputData() interface{} {
-    return d.Text
+	return d.Text
 }
 
 type Location struct {
-    Lat float32
-    Lng float32
+	Lat float32
+	Lng float32
 }
